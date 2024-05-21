4228f02879a1df2184c5837d31d0a418af351b6f
Anish Paudel <rpaudel379@gmail.com>
2024-05-21 08:51:39 +0530

    adding user profile form feature

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 type Props = {
-  children: React.ReactNode;
+  children: React.ReactNode; 
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
